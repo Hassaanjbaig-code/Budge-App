85eae3c033f335750e6918b77aaa2e1e7c94b45f
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-27 18:30:59 +0500

    Fix the linter

--- spec/views/group_spec.rb
+++ spec/views/group_spec.rb
@@ -20,7 +20,6 @@ RSpec.describe 'Groups', type: :system do
       fill_in 'Name', with: 'Group 1'
       fill_in 'Icon', with: 'https://images.unsplash.com/photo-1583840726841-c35239a17b23?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
 
-
       click_on 'Create Catagories'
 
       sleep 1
