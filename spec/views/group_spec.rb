19b0f29a4b6455f1e872cbae61ad88df11520933
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-27 15:40:32 +0500

    update the data

---
+++ spec/views/group_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+Rspect.describe 'Group', type: :system do
+    let (:user) { User.create(name: 'John', email: "foo@yopmail.com", password: "1234567") }
+ describe 'Group' do
+    
+end
+end
