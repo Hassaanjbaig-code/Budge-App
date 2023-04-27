9e0365c15a315d89edac36cb16a9ae1e0b383b6f
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-04-27 13:51:23 +0500

    Add more style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
   end
+
+  def after_sign_in_path_for(resource)
+    groups_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
+  root "welcomes#index"
   devise_for :users
-  # root "articles#index"
-  get "/groups/index", to: "groups#index"
   resources :groups, only: [:index, :new, :create] do
     resources :payments, only: [:index, :new, :create]
   end
